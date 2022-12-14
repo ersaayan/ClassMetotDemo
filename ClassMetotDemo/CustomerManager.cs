c4c23262c983f6854f0cdb68d2016d4143a4a87b
Eren Ali Şavk <esavk22@posta.pau.edu.tr>
2022-12-14 20:42:18 +0300

    Class Method Practice

---
+++ ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    public class CustomerManager
+    {
+        public void Add(Customer customer)
+        {
+            Console.WriteLine("Müşteri EKlendi. Eklenen müşterinin adı: " + customer.Name);
+        }
+        public void Update(Customer customer) 
+        {
+            Console.WriteLine("Müşteri güncellendi. Güncellenen müşterinin adı: " + customer.Name);
+        }
+        public void Delete(Customer customer) 
+        {
+            Console.WriteLine("Müşteri silindi. Silinen müşterinin adı: " + customer.Name);
+        }
+        
+
+    }
+}
--- ClassMetotDemo/Program.cs
+++ ClassMetotDemo/Program.cs
@@ -6,7 +6,33 @@ namespace ClassMetotDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Customer customer1 = new Customer();
+            customer1.Id = 1;
+            customer1.Name = "Eren Ali";
+            customer1.Address = "Yeni mah.";
+            customer1.City = "Aydın";
+
+            Customer customer2 = new Customer();
+            customer2.Id = 2;
+            customer2.Name = "Azra Betül";
+            customer2.Address = "Yeni mah.";
+            customer2.City = "Afyon";
+
+            Customer[] customers = new Customer[] { customer1, customer2 };
+            foreach (var customer in customers)
+            {
+                Console.WriteLine(customer.Name);
+                Console.WriteLine(customer.Address);
+                Console.WriteLine(customer.City);
+                Console.WriteLine("  ");
+            }
+            Console.WriteLine("--------Customer Metodları--------");
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add(customer1);
+            customerManager.Update(customer1);
+            customerManager.Delete(customer1);
+
+
         }
     }
 }
